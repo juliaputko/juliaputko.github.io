26c5ccd0a66cc2a2f32e752195fe1cca58ad849a
Julia Putko <81587103+juliaputko@users.noreply.github.com>
2025-10-31 18:28:29 -0700

    WIP (#3)
    * Added project info
    
    * Updated project section

--- js/load.js
+++ js/load.js
@@ -26,3 +26,10 @@ window.addEventListener('load', function() {
   history.scrollRestoration = 'manual';
 }
 
+
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('navLinks');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
